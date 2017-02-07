6fecca481f34f7d8509a6b739c228601099274ce
Evan Mortimore <evan-github@madstyle.net>
2017-02-06 19:59:00 -0800

    Starting the GPIO stuff to make an LED blink.

--- blinker/blinker.cpp
+++ blinker/blinker.cpp
@@ -1,4 +1,15 @@
-#include <ostream>
+//#define PERIPHERAL_BASE 0x20000000 // for rpi 1
+
+#define PERIPHERAL_BASE 0x3F000000 // for rpi 2&3
+
+enum Gpio {
+	GPFSEL0=PERIPHERAL_BASE + 0x200000,
+	GPFSEL1,
+	GPFSEL2,
+	GPFSEL3,
+	GPFSEL4,
+	GPFSEL5
+};
 
 int main(int argc, char *argv[]) {
 	volatile int a = 0;
@@ -7,4 +18,10 @@ int main(int argc, char *argv[]) {
 		a++;
 		
 	return 0;
+}
+
+extern "C" {
+	void _exit(int x) {
+		while(1);
+	}
 }
