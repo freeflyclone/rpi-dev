20347ff007fda3e26c2202261bdf9f77cb67f57d
Evan Mortimore <evan-github@madstyle.net>
2017-02-11 18:32:30 -0800

    Add ip= to bootargs, eth0 starts up with IP addr

--- linux/tftpboot.cmd
+++ linux/tftpboot.cmd
@@ -1,5 +1,7 @@
 tftp ${kernel_addr_r} zImage
 tftp ${ramdisk_addr_r} uInitrd
 tftp ${fdt_addr_r} bcm2836-rpi-2-b.dtb 
-setenv bootargs console=ttyAMA0,115200 earlyprintk root=/dev/root rootwait panic=10
+#
+setenv bootargs console=ttyAMA0,115200 earlyprintk root=/dev/root rootwait panic=10 ip=${ipaddr}:${serverip}::255.255.255.0:rpi:eth0::8.8.4.4:8.8.8.8
+#
 bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
