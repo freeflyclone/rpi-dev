bce3d8659d06724428cf56cfbb7699955b850d6d
Evan Mortimore <evan-github@madstyle.net>
2017-02-20 09:38:04 -0800

    Enhancing for kernel modules development

--- linux/tftpboot.cmd
+++ linux/tftpboot.cmd
@@ -2,6 +2,8 @@ tftp ${kernel_addr_r} zImage
 tftp ${ramdisk_addr_r} uInitrd
 tftp ${fdt_addr_r} bcm2836-rpi-2-b.dtb 
 #
+setenv ipaddr dhcp
+setenv serverip 10.1.1.1
 setenv bootargs console=ttyAMA0,115200 earlyprintk root=/dev/root rootwait panic=10 ip=${ipaddr}:${serverip}::255.255.255.0:rpi:eth0::8.8.4.4:8.8.8.8
 #
 bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
